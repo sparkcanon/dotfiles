480a9281445232b0058e834a7afb61088a9f03fa
Pratik <pratik.borde@sapient.com>
2020-04-09 21:40:47 +0100

    check if git and if brew and install only if brew

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,101 +1,109 @@
 #!/bin/bash
 
 config() {
-   /usr/bin/git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME" $@
+	/usr/bin/git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME" $@
 }
 
 has() {
-  type "${1:?too few arguments}" &>/dev/null
+	type "${1:?too few arguments}" &>/dev/null
 }
 
 echoSeparator() {
-  echo --------------------------------------------------
+	echo --------------------------------------------------
 }
 
 echoSeparator
 
+if ! has 'git'; then
+	sudo apt install git-all
+fi
+
 # Install brew
 if ! has 'brew'; then
-  echo Installing brew.. 🍺
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	echo Installing brew.. 🍺
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 echoSeparator
 
-if has 'fish'; then
-  brew install fish
-  curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-  echo /usr/local/bin/fish | sudo tee -a /etc/shells
-  chsh -s /usr/local/bin/fish
+if has 'brew'; then
+
+	if has 'fish'; then
+		brew install fish
+		curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+		echo /usr/local/bin/fish | sudo tee -a /etc/shells
+		chsh -s /usr/local/bin/fish
+	fi
+
+	echoSeparator
+
+	# Git
+	if has 'git'; then
+		echo Git is not installed
+		brew install git
+	fi
+
+	echoSeparator
+
+	# Tmux
+	if ! has 'tmux'; then
+		echo Installing tmux.. 🖥️
+		brew install tmux
+	fi
+
+	echoSeparator
+
+	# Tmuxinator
+	if ! has 'tmuxinator'; then
+		echo Installing tmuxinator..
+		brew install tmuxinator
+	fi
+
+	echoSeparator
+
+	# Kitty
+	if ! has 'kitty'; then
+		echo Installing kitty.. 🐈
+		brew cask install kitty
+	fi
+
+	echoSeparator
+
+	# Alacritty
+	if ! has 'alacritty'; then
+		echo Installing alacritty..
+		brew cask install alacritty
+	fi
+
+	echoSeparator
+
+	# Neovim
+	if ! has 'neovim'; then
+		echo Installing neovim.. 📟
+		brew install neovim
+	fi
+
+	echoSeparator
+
+	# Vim
+	echo Installing latest vim.. 📟
+	brew install vim
+
+	echoSeparator
+
+	# Autojump
+	if ! has 'autojump'; then
+		echo Installing autojump..
+		brew install autojump
+	fi
+
+	echoSeparator
+
+	# brew clean up
+	echo Cleaning up brew 🧹
+	brew cleanup
+
 fi
-
-echoSeparator
-
-# Git
-if has 'git'; then
-  echo Git is not installed
-  brew install git
-fi
-
-echoSeparator
-
-# Tmux
-if ! has 'tmux'; then
-  echo Installing tmux.. 🖥️
-  brew install tmux
-fi
-
-echoSeparator
-
-# Tmuxinator
-if ! has 'tmuxinator'; then
-  echo Installing tmuxinator..
-  brew install tmuxinator
-fi
-
-echoSeparator
-
-# Kitty
-if ! has 'kitty'; then
-  echo Installing kitty.. 🐈
-  brew cask install kitty
-fi
-
-echoSeparator
-
-# Alacritty
-if ! has 'alacritty' ; then
-  echo Installing alacritty..
-  brew cask install alacritty
-fi
-
-echoSeparator
-
-# Neovim
-if ! has 'neovim'; then
-  echo Installing neovim.. 📟
-  brew install neovim
-fi
-
-echoSeparator
-
-# Vim
-echo Installing latest vim.. 📟
-brew install vim
-
-echoSeparator
-
-# Autojump
-if ! has 'autojump'; then
-  echo Installing autojump..
-  brew install autojump
-fi
-
-echoSeparator
-
-# brew clean up
-echo Cleaning up brew 🧹
-brew cleanup
 
 echoSeparator
 
@@ -110,15 +118,15 @@ echo Configuring dotfiles..
 mkdir -p .config-backup
 config checkout
 if [ $? = 0 ]; then
-  echo Checked out config
-  else
-    echo Backing up pre-existing dot files
-    config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
+	echo Checked out config
+else
+	echo Backing up pre-existing dot files
+	config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi
 config checkout
 config config status.showUntrackedFiles no/bin/bash
 
-echo alias config="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME" >> "$HOME"/.bashrc
+echo alias config="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME" >>"$HOME"/.bashrc
 source "$HOME"/.bashrc
 
 config submodule init
