c1afbc7b53557f875d46466f7bb43f523808510b
Pratik <pratik.borde@sapient.com>
2020-09-20 15:54:28 +0100

    chore: flutter path, nvim & vim updates

--- .config/fish/conf.d/so.fish
+++ .config/fish/conf.d/so.fish
@@ -4,5 +4,8 @@ set -U fish_user_paths /usr/local/share/git-core/contrib/git-jump
 # Homebrew
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
+# Flutter
+set -g fish_user_paths "/Users/praborde/Documents/flutter/bin/" $fish_user_paths
+
 # Source autojump
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
