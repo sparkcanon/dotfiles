b1dcb028105a31e2d1a203f5a384262dc71650c7
Pratik <pratik.borde@sapient.com>
2020-09-22 11:14:45 +0100

    chore: move TERM to fish, kitty colors, nvim latest commit

--- .config/fish/conf.d/vars.fish
+++ .config/fish/conf.d/vars.fish
@@ -3,6 +3,7 @@ set -U FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 set -U FZF_DEFAULT_OPTS "--ansi"
 set -U EDITOR "vim"
 set -U VISUAL "vim"
+set -U TERM "xterm-256color"
 
 set -x MANPAGER "vim -M +MANPAGER -"
 
